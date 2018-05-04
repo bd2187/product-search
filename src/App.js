9d66bd2e86b95cba0ff7cb17fc0e201d6aa0db35
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-05-03 17:39:08 -0700

    Extract PRODUCTS array to data folder. Create static version of components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,94 @@
 import React, { Component } from "react";
+import PRODUCTS from "./data/products";
+
+class ProductCategoryRow extends Component {
+  render() {
+    const category = this.props.category;
+    return (
+      <tr>
+        <th colSpan="2">{category}</th>
+      </tr>
+    );
+  }
+}
+
+class ProductRow extends Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ? (
+      product.name
+    ) : (
+      <span style={{ color: "red" }}>{product.name}</span>
+    );
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    );
+  }
+}
+
+class ProductTable extends Component {
+  render() {
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach(function(product) {
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category}
+          />
+        );
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" /> Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+class FilterableProductTable extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    return <p>test</p>;
+    return <FilterableProductTable products={PRODUCTS} />;
   }
 }
 
